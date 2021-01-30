48687135998bb161f4c27ea84d6c4fd5278881bd
jovial-application-1943 <fatima.katrib@lau.edu>
2021-01-30 19:05:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let num1  = 4
 let num2 = 13
 const multiply = num1 * num2
 const random = Math.floor( Math.random() * 1000 ) + 1
-let num4=16
-
-const mod = 16 % 6
+let num3 = 6
+let num4 = 16
+const mod = num3 % num4
 const max = Math.max( 5, 10, 15, 20 )
